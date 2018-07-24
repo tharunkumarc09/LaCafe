1d92d9e90dbe84a2ef0f73d04bbc766a05d3ccfb
tharunkumarc09 <37200035+tharunkumarc09@users.noreply.github.com>
2018-07-24 10:57:37 -0400

    Update rds-snapshot-cleanup.sh

--- rds-snapshot-cleanup.sh
+++ rds-snapshot-cleanup.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
-date1=$(date --date="1 days ago" +%F)
-date2=$(date --date="2 days ago" +%F)
-date3=$(date --date="3 days ago" +%F)
-echo $date1 $date2 $date3
+date=$(date +%F)
+# date2=$(date --date="2 days ago" +%F)
+# date3=$(date --date="3 days ago" +%F)
+echo $date
 
 get_rds() {
-        aws rds describe-db-snapshots  --query "DBSnapshots[].[DBSnapshotArn]" --output text
+        aws rds describe-db-snapshots --snapshot-type manual --query "DBSnapshots[].[DBSnapshotArn]" --output text
 }
 
 remove_rds_using_tags() {
         expire=$(aws rds list-tags-for-resource --resource-name "$1" --query 'TagList[?Key==`expiration`].Value')
-        expire=$(echo $expire | cut -c4-)
-        expire=${expire:0:-3}
-        echo $expire
-        if [ "$expire" == "$date1" ]
+        if [[ ! -z $expire ]]
         then
-           echo "matching"
-           snapshotid=$(echo "$1" | rev | cut -d':' -f1 | rev)
-           echo $snapshotid
-           aws rds delete-db-snapshot --db-snapshot-identifier $snapshotid
+                expire=$(echo $expire | cut -c4-)
+                echo $expire
+                expire=${expire:0:-3}
+                echo $expire
+                        if [ "$expire" == "$date" ]
+                        then
+                                echo "Expiration date matching the current date.. Proceeding with Purging"
+                                snapshotid=$(echo "$1" | rev | cut -d':' -f1 | rev)
+                                echo $snapshotid
+                                #   aws rds delete-db-snapshot --db-snapshot-identifier $snapshotid
+                        else
+                                echo "Expiration date not matching the current date"
+                        fi
         else
-                echo "Not matching"
+                echo "No expiration tag for the snapshot"
         fi
+
 }
 
 for id in $(get_rds)
